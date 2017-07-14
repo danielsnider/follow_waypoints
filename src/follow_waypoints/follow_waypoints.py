fd39c112d0fa8cc2b187f03599b416cbb0c8609b
danielsnider <danielsnider12@gmail.com>
2017-07-14 19:45:21 -0400

    Added support for emptying the waypoint queue

--- src/follow_waypoints/follow_waypoints.py
+++ src/follow_waypoints/follow_waypoints.py
@@ -9,6 +9,7 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from geometry_msgs.msg import PoseWithCovarianceStamped, PoseArray
 from std_msgs.msg import Empty
 
+waypoints = []
 
 class FollowPath(State):
     def __init__(self):
@@ -21,8 +22,14 @@ class FollowPath(State):
         rospy.loginfo('Connected to move_base.')
 
     def execute(self, userdata):
+        global waypoints
         # Execute waypoints each in sequence
-        for waypoint in userdata.waypoints:
+        for waypoint in waypoints:
+            # Break if preempted
+            if waypoints == []:
+                rospy.loginfo('The waypoint queue has been reset.')
+                break
+            # Otherwise publish next waypoint as goal
             goal = MoveBaseGoal()
             goal.target_pose.header.frame_id = self.frame_id
             goal.target_pose.pose.position = waypoint.pose.pose.position
@@ -47,36 +54,59 @@ class GetPath(State):
         # Create publsher to publish waypoints as pose array so that you can see them in rviz, etc.
         self.poseArray_publisher = rospy.Publisher('/waypoints', PoseArray, queue_size=1)
 
+        # Start thread to listen for reset messages to clear the waypoint queue
+        def wait_for_path_reset():
+            """thread worker function"""
+            global waypoints
+            while not rospy.is_shutdown():
+                data = rospy.wait_for_message('/path_reset', Empty)
+                rospy.loginfo('Recieved path RESET message')
+                self.initialize_path_queue()
+                rospy.sleep(3) # Wait 3 seconds because `rostopic echo` latches
+                               # for three seconds and wait_for_message() in a
+                               # loop will see it again.
+        reset_thread = threading.Thread(target=wait_for_path_reset)
+        reset_thread.start()
+
+    def initialize_path_queue(self):
+        global waypoints
+        waypoints = [] # the waypoint queue
+        # publish empty waypoint queue as pose array so that you can see them the change in rviz, etc.
+        self.poseArray_publisher.publish(convert_PoseWithCovArray_to_PoseArray(waypoints))
+
     def execute(self, userdata):
-        waypoints = []
+        global waypoints
+        self.initialize_path_queue()
         self.path_ready = False
 
         # Start thread to listen for when the path is ready (this function will end then)
         def wait_for_path_ready():
             """thread worker function"""
             data = rospy.wait_for_message('/path_ready', Empty)
-            rospy.loginfo('Recieved path ready message')
+            rospy.loginfo('Recieved path READY message')
             self.path_ready = True
-        t = threading.Thread(target=wait_for_path_ready)
-        t.start()
+        ready_thread = threading.Thread(target=wait_for_path_ready)
+        ready_thread.start()
 
-        # Wait for published waypoints
         topic = "/initialpose"
         rospy.loginfo("Waiting to recieve waypoints via Pose msg on topic %s" % topic)
         rospy.loginfo("To start following waypoints: 'rostopic pub /path_ready std_msgs/Empty -1'")
+
+        # Wait for published waypoints
         while not self.path_ready:
             try:
                 pose = rospy.wait_for_message(topic, PoseWithCovarianceStamped, timeout=1)
             except rospy.ROSException as e:
                 if 'timeout exceeded' in e.message:
-                    continue
+                    continue  # no new waypoint within timeout, looping...
                 else:
                     raise e
             rospy.loginfo("Recieved new waypoint")
             waypoints.append(pose)
+            # publish waypoint queue as pose array so that you can see them in rviz, etc.
             self.poseArray_publisher.publish(convert_PoseWithCovArray_to_PoseArray(waypoints))
-            userdata.waypoints = waypoints
 
+        # Path is ready! return success and move on to the next state (FOLLOW_PATH)
         return 'success'
 
 class PathComplete(State):
@@ -93,7 +123,6 @@ def main():
     rospy.init_node('follow_waypoints')
 
     sm = StateMachine(outcomes=['success'])
-    sm.userdata.waypoints = []
 
     with sm:
         StateMachine.add('GET_PATH', GetPath(),
